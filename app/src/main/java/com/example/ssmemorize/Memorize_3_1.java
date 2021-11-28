5cf370086c01a751b1a6c105ded83e57ab7813b0
rladbflaz <youro1228@naver.com>
2021-11-28 16:11:15 +0900

    자바 파일 변경

--- app/src/main/java/com/example/ssmemorize/Memorize_3_1.java
+++ app/src/main/java/com/example/ssmemorize/Memorize_3_1.java
@@ -3,12 +3,33 @@ package com.example.ssmemorize;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Memorize_3_1 extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memorize31);
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter monthAdapter = ArrayAdapter.createFromResource(this, R.array.word_number, android.R.layout.simple_spinner_dropdown_item);
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(monthAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }
+        });
     }
 }
--- app/src/main/java/com/example/ssmemorize/Memorize_3_2.java
+++ app/src/main/java/com/example/ssmemorize/Memorize_3_2.java
@@ -2,7 +2,11 @@ package com.example.ssmemorize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Memorize_3_2 extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class Memorize_3_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memorize32);
+
+        ImageButton Button32 =  findViewById(R.id.btn32);
+        Button32.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Memorize_3_main.class);
+                startActivity(intent);
+            }
+        });
     }
 }
