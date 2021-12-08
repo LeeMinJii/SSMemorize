813ebb854b089cd560097be9ca1ae212f857bf74
yes--ee <yes0112@naver.com>
2021-12-09 01:29:21 +0900

    객관식 게임 화면 전환

--- app/src/main/java/com/example/ssmemorize/Game_4_1_2.java
+++ app/src/main/java/com/example/ssmemorize/Game_4_1_2.java
@@ -3,11 +3,15 @@ package com.example.ssmemorize;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Game_4_1_2 extends AppCompatActivity {
+
+    Button btn_game1_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,16 @@ public class Game_4_1_2 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btn_game1_start = (Button) findViewById(R.id.btn_game1_start);
+        btn_game1_start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Game_4_1_3.class);
+                startActivity(intent);
             }
         });
     }
